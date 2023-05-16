932b7ea26d35d62c1dbf6ae15348c64fff68d8e0
jeonghunkim17 <jhmaster17@gmail.com>
2023-05-17 10:45:58 +1200

    Upload images and Update files

--- js/background.js
+++ js/background.js
@@ -1,4 +1,17 @@
-const images = ["0.jpeg", "1.jpeg", "2.jpeg"];
+const images = [
+  "0.jpg",
+  "1.jpg",
+  "2.jpg",
+  "3.jpg",
+  "4.jpg",
+  "5.jpg",
+  "6.jpg",
+  "7.jpg",
+  "8.jpg",
+  "9.jpg",
+  "10.jpg",
+  "11.jpg",
+];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
